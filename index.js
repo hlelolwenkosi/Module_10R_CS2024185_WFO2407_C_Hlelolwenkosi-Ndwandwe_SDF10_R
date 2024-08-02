7b753b21f6ab983e442da23b3709cf89af204b38
hlelolwenkosi <1071hf@gmail.com>
2024-08-02 08:51:16 +0200

    Added firebase code in js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: process.env.DATABASE_URL
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
+
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
