f25bf59f12132aafd191197747183c71fbd85251
hlelolwenkosi <1071hf@gmail.com>
2024-08-02 11:27:15 +0200

    Added innerHTML to append li to ul

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getDatabase,
-        ref
-        push } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase ref push } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 
-
-const firebaseConfig = {
-    databaseURL: process.env.DATABASE_URL
+const appSettings={
+    databaseURL: "https://realtime-database-bbc4f-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const referenceInDB = ref(database, "leads")
+const shoppingListInDB = ref(database, "shoppingList")
 
 
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+
+    push(shoppingListInDB, inputValue)
+
+    inputFieldEl.value = ""
     
-    console.log(inputValue)
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 })
+    
+    
+
+
+
+
+
 
